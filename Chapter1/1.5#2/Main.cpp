8b7314539de4da6456df1cedc2491ab13a1ebddb
H0RIS <H0RIS@outlook.com>
2024-09-04 13:53:52 +0200

    1.5#2

--- Chapter1/1.5#2/Main.cpp
+++ Chapter1/1.5#2/Main.cpp
@@ -1,8 +1,13 @@
-import std;
+#include <iostream>
 
 int main()
 {
-
+    std::cout << "Enter 3 numbers: ";
+    int x1{};
+    int x2{};
+    int x3{};
+    std::cin >> x1 >> x2 >> x3;
+    std::cout << "You entered " << x1 << ", " << x2 << ", and " << x3 << "." << '\n';
 
     return 0;
 }
