9db1a1cd3603ca21da11a195f74fbd5c7d33cf14
H0RIS <H0RIS@outlook.com>
2024-09-09 13:09:53 +0200

    2.4#5

--- Chapter2/2.4#5/Main.cpp
+++ Chapter2/2.4#5/Main.cpp
@@ -1,8 +1,24 @@
 import std;
 
+int GetInt()
+{
+    std::cout << "Enter an int value:\n";
+    int input{};
+    std::cin >> input;
+
+    return input;
+}
+
+int DoubleNumber(int num)
+{
+    return num * 2;
+}
+
 int main()
 {
+    int input{ GetInt() };
 
+    std::cout << "Double your number is: " << DoubleNumber(input);
 
     return 0;
 }
