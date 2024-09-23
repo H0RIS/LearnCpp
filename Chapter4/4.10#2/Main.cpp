93f636693f0dc0a0fec3bf846dfc97d7035c5619
H0RIS <H0RIS@outlook.com>
2024-09-23 19:54:58 +0200

    4.10#2

--- Chapter4/4.10#2/Main.cpp
+++ Chapter4/4.10#2/Main.cpp
@@ -1,8 +1,30 @@
 import std;
 
+bool isPrime(int val)
+{
+    if (val == 2 || val == 3 || val == 5 || val == 7)
+    {
+        return true;
+    }
+    else 
+    { 
+        return false;
+    }
+}
 int main()
 {
+    std::cout << "Enter a digit(0-9): ";
+    int val{};
+    std::cin >> val;
 
+    if (isPrime(val))
+    {
+        std::cout << "The digit is prime";
+    }
+    else
+    {
+        std::cout << "The digit is not prime";
+    }
 
     return 0;
 }
