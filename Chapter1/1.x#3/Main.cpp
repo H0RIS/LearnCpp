236765f5338a8b41969376c441b18f62ebdc9614
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-08 17:56:58 +0100

    1.x#3

--- Chapter1/1.x#3/Main.cpp
+++ Chapter1/1.x#3/Main.cpp
@@ -1,3 +1,8 @@
+/*
+Write a program that asks the user to enter a number, and then enter a second number.
+The program should tell the user what the result of adding and subtracting the two numbers is
+*/
+
 #include <iostream>
 
 int main()
