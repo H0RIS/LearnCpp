e5d169bec58612b65c5cc0288fc5f5c7ad389a29
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-08 18:10:55 +0100

    added exercise text

--- Chapter1/1.10#1/Main.cpp
+++ Chapter1/1.10#1/Main.cpp
@@ -1,8 +1,8 @@
 /*
-Write a program so that it outputs like this:
-    Enter an integer: 4
-    Double 4 is: 8
-    Triple 4 is: 12
+* Write a program so that it outputs like this:
+*   Enter an integer: 4
+*   Double 4 is: 8
+*   Triple 4 is: 12
 */
 
 #include <iostream>
