6139d9ebdea74be2cd24311319bdd0b597fc400f
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-09 10:39:09 +0100

    added exercise text

--- Chapter4/4.10#2/Main.cpp
+++ Chapter4/4.10#2/Main.cpp
@@ -1,3 +1,9 @@
+/*
+* Write a program that asks the user to enter a number 0 through 9 (inclusive).
+* If the user enters a number within this range that is prime (2, 3, 5, or 7),
+* print “The digit is prime”. Otherwise, print “The digit is not prime”.
+*/
+
 import std;
 
 bool isPrime(int val)
