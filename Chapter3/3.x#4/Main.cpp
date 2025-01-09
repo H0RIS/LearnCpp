ae62459f9086269a65b40996d58c3d97a75d60ff
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-09 10:36:56 +0100

    added exercise text

--- Chapter3/3.x#4/Main.cpp
+++ Chapter3/3.x#4/Main.cpp
@@ -1,3 +1,8 @@
+/*
+* Extra credit: The following program is supposed to add two numbers, but doesn’t work correctly.
+* Use the integrated debugger to step through this program. For inputs, enter 8 and 4. Based on the information you learn, fix the following program:
+*/
+
 #include <iostream>
 
 int readNumber()
