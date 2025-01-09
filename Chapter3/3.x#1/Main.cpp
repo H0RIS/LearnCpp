391a9e734ab0cc36e10d5579e4db9b7cbf336209
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-09 10:34:01 +0100

    added exercise text

--- Chapter3/3.x#1/Main.cpp
+++ Chapter3/3.x#1/Main.cpp
@@ -1,3 +1,34 @@
+/*
+* The following program is supposed to add two numbers, but doesn’t work correctly.
+* Use the integrated debugger to step through this program and watch the value of x.
+* Based on the information you learn, fix the following program:
+*
+* #include <iostream>
+* 
+* int readNumber(int x)
+* {
+* 	std::cout << "Please enter a number: ";
+* 	std::cin >> x;
+* 	return x;
+* }
+*
+* void writeAnswer(int x)
+* {
+* 	std::cout << "The sum is: " << x << '\n';
+* }
+*
+* int main()
+* {
+* 	int x {};
+* 	readNumber(x);
+* 	x = x + readNumber(x);
+* 	writeAnswer(x);
+*
+* 	return 0;
+* }
+*/
+
+
 #include <iostream>
 
 int readNumber()
