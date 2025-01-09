dc4ed023a0ba5d48cb0521c003a89f229a44e3b1
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-09 10:36:07 +0100

    added exercise text

--- Chapter3/3.x#3/Main.cpp
+++ Chapter3/3.x#3/Main.cpp
@@ -1,3 +1,8 @@
+/*
+* What does the call stack look like in the following program when the point of execution is on line 4? 
+* Only the function names are needed for this exercise, not the line numbers indicating the point of return. 
+*/
+
 #include <iostream>
 
 void d()
