5ba243fdfc6cf8b61b89248b945034581a408c59
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-10 23:28:14 +0100

    5.7#1

--- Chapter5/5.7#1/Main.cpp
+++ Chapter5/5.7#1/Main.cpp
@@ -9,7 +9,16 @@ import std;
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Enter your full name: ";
+    std::string name{};
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Enter your age: ";
+    int age{};
+    std::cin >> age;
+
+    std::cout << "Your age + length of name is: " <<(age + std::ssize(name));
+
 
     return 0;
 }
