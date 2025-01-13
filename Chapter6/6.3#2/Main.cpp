afe1c09b55cc25c71ec143ee8e4f60b230cc5122
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-13 21:55:54 +0100

    6.3#2

--- Chapter6/6.3#2/Main.cpp
+++ Chapter6/6.3#2/Main.cpp
@@ -1,8 +1,38 @@
+/*
+* Write a program that asks the user to input an integer, and tells the user whether the number is even or odd.
+* Write a constexpr function called isEven() that returns true if an integer passed to it is even, and false otherwise.
+* Use the remainder operator to test whether the integer parameter is even.
+* Make sure isEven() works with both positive and negative numbers.
+*/
+
 import std;
+
+constexpr bool isEven(int x)
+{
+    if (x % 2)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Eneter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    if (isEven(x))
+    {
+        std::cout << x << " is even";
+    }
+    else
+    {
+        std::cout << x << " is odd";
+    }
 
     return 0;
 }
