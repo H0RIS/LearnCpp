c7c7c374c417f980f104cbf84f751c4e2f961f49
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-13 22:10:43 +0100

    6.x

---
+++ Chapter6/6.x/Main.cpp
@@ -0,0 +1,64 @@
+/*
+* Complete the following program:
+*/
+
+#include <iostream>
+
+// Write the function getQuantityPhrase() here
+std::string_view getQuantityPhrase(int x)
+{
+    if (x < 0)
+    {
+        return "negative";
+    }
+    
+    if (x == 0)
+    {
+        return "no";
+    }
+
+    if (x == 1)
+    {
+        return "a single";
+    }
+    
+    if (x == 2)
+    {
+        return "a couple of";
+    }
+
+    if (x == 3)
+    {
+        return "a few";
+    }
+
+    return "many";
+}
+
+// Write the function getApplesPluralized() here
+std::string_view getApplesPluralized(int x)
+{
+    if (x == 1)
+    {
+        return "apple";
+    }
+    else
+    {
+        return "apples";
+    }
+}
+
+
+int main()
+{
+    constexpr int maryApples{ 3 };
+    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+
+    std::cout << "How many apples do you have? ";
+    int numApples{};
+    std::cin >> numApples;
+
+    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+
+    return 0;
+}
