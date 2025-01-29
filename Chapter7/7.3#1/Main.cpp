a6892f8e5aa333cca2bbd9d4faf4b16b631993d9
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-29 12:36:11 +0100

    7.3#1

--- Chapter7/7.3#1/Main.cpp
+++ Chapter7/7.3#1/Main.cpp
@@ -2,7 +2,23 @@ import std;
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Enter 2 ints: \n";
+    int smaller{};
+    int larger{};
+    std::cin >> smaller >> larger;
+
+    if (larger < smaller)
+    {
+        std::cout << "Swapping the values\n";
+
+        int temp{ larger };
+        larger = smaller;
+        smaller = temp;
+    }
+
+    std::cout << "The smaller value is " << smaller << '\n';
+    std::cout << "The larger value is " << larger << '\n';
+
 
     return 0;
 }
