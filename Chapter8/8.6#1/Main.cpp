32967a8abf24201cc3f44a946cd0a4028346499d
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-29 18:39:14 +0100

    8.6#1

--- Chapter8/8.6#1/Main.cpp
+++ Chapter8/8.6#1/Main.cpp
@@ -1,8 +1,61 @@
+/*
+Write a function called calculate() that takes two integers and a char representing one of the following mathematical operations: +, -, *, /, or % (remainder).
+Use a switch statement to perform the appropriate mathematical operation on the integers, and return the result.
+If an invalid operator is passed into the function, the function should print an error message.
+For the division operator, do an integer division, and don’t worry about divide by zero.
+*/
+
 import std;
 
+void calculate(int x, int y, char op)
+{
+    switch (op)
+    {
+    case'+':
+    {
+        std::cout << x << " + " << y << " = " << x + y << '\n';
+        break;
+    }
+    case'-':
+    {
+        std::cout << x << " - " << y << " = " << x - y << '\n';
+        break;
+    }
+    case'*':
+    {
+        std::cout << x << " * " << y << " = " << x * y << '\n';
+        break;
+    }
+    case'/':
+    {
+        std::cout << x << " / " << y << " = " << x / y << '\n';
+        break;
+    }
+    case'%':
+    {
+        std::cout << x << " % " << y << " = " << x % y << '\n';
+        break;
+    }
+    default:
+        std::cout << "invalid input\n";
+        break;
+    }
+}
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Enter an integer: ";
+    int y{};
+    std::cin >> y;
+
+    std::cout << "Enter an operator(+, -, *, /, %): ";
+    char op{};
+    std::cin >> op;
+
+    calculate(x, y, op);
 
     return 0;
 }
