f0f8359592c9c15415a8ae42f625e9a23d5675b8
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-30 19:56:26 +0100

    8.8#3

--- Chapter8/8.8#3/Main.cpp
+++ Chapter8/8.8#3/Main.cpp
@@ -6,25 +6,19 @@ Invert the nested loops example so it prints the following:
 
 int main()
 {
-    // outer loops between 1 and 5
     int outer{ 1 };
     while (outer <= 5)
     {
-        // For each iteration of the outer loop, the code in the body of the loop executes once
-
-        // inner loops between 1 and outer
-        // note: inner will be created and destroyed at the end of the block
-        int inner{ 1 };
-        while (inner <= outer)
+        int inner{ 5 };
+        while (inner >= outer)
         {
             std::cout << inner << ' ';
-            ++inner;
+            --inner;
         }
 
-        // print a newline at the end of each row
         std::cout << '\n';
         ++outer;
-    } // inner destroyed here
+    }
 
     return 0;
 }
