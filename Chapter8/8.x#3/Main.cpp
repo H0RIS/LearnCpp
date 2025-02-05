8003265689a4385563b2af3be8c39b318810ddfa
H0RIS@outlook.com <H0RIS@outlook.com>
2025-02-05 13:48:45 +0100

    init

---
+++ Chapter8/8.x#3/Main.cpp
@@ -0,0 +1,13 @@
+/*
+Implement a game of Hi-Lo. First, your program should pick a random integer between 1 and 100.
+The user is given 7 tries to guess the number.
+*/
+
+import std;
+
+int main()
+{
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
