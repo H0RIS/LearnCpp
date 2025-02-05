53799dbc557e08b4d0745c434b965ef068bca1a9
H0RIS@outlook.com <H0RIS@outlook.com>
2025-02-05 13:45:47 +0100

    fix

--- Chapter8/8.x#2/Main.cpp
+++ Chapter8/8.x#2/Main.cpp
@@ -14,8 +14,16 @@ bool isPrime(int x)
     {
         return false;
     }
+    if (x == 2)
+    {
+        return true;
+    }
+    if (x % 2 == 0)
+    {
+        return false;
+    }
 
-    for (size_t i{ 2 }; i < x; i++)
+    for (size_t i{ 3 }; i * i <= x; i += 2)
     {
         if (x % i == 0)
         {
