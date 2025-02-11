0b7d70214ccfe309e500c98d75f77bfd3c8b2e8e
H0RIS@outlook.com <H0RIS@outlook.com>
2025-02-11 20:35:01 +0100

    11.x#2.3

--- Chapter11/11.x#2/Main.cpp
+++ Chapter11/11.x#2/Main.cpp
@@ -3,6 +3,8 @@
 
 #2 Write a function template named mult() that allows the user to multiply one value of any type (first parameter) and an integer (second parameter).
 The second parameter should not be a template type. The function should return the same type as the first parameter.
+
+#3 Write a function template named sub() that allows the user to subtract two values of different types.
 */
 
 #include <iostream>
@@ -18,6 +20,11 @@ auto mult(auto x, auto y)
     return x * y;
 }
 
+auto sub(auto x, auto y)
+{
+    return x - y;
+}
+
 int main()
 {
     std::cout << add(2, 3) << '\n';
@@ -25,5 +32,10 @@ int main()
 
     std::cout << mult(2, 3) << '\n';
     std::cout << mult(1.2, 3) << '\n';
+
+    std::cout << sub(3, 2) << '\n';
+    std::cout << sub(3.5, 2) << '\n';
+    std::cout << sub(4, 1.5) << '\n';
+
     return 0;
 }
