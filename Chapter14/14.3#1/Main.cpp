3ada6cae39da47b3aa24b8542a89870d1ede7ea9
H0RIS@outlook.com <H0RIS@outlook.com>
2025-02-18 13:02:50 +0100

    14.3#1

--- Chapter14/14.3#1/Main.cpp
+++ Chapter14/14.3#1/Main.cpp
@@ -1,8 +1,33 @@
+/*
+Create a struct called IntPair that holds two integers.
+Add a member function named print that prints the value of the two integers.
+
+
+*/
+
 import std;
+
+struct IntPair
+{
+    int x{};
+    int y{};
+
+    void Print()
+    {
+        std::cout << "Pair(" << x << ", " << y << ")\n";
+    }
+};
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    IntPair p1{ 1, 2 };
+    IntPair p2{ 3, 4 };
+
+    std::cout << "p1: ";
+    p1.Print();
+
+    std::cout << "p2: ";
+    p2.Print();
 
     return 0;
 }
