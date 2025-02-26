9451a1e548d4c4313438737b2d5e0bec14688071
H0RIS@outlook.com <H0RIS@outlook.com>
2025-02-26 16:11:09 +0100

    15.5#1

--- Chapter15/15.5#1/Main.cpp
+++ Chapter15/15.5#1/Main.cpp
@@ -1,8 +1,50 @@
-import std;
+/*
+Write a class template named Triad that has 3 private data members with independent type template parameters.
+The class should have a constructor, access functions, and a print() member function that is defined outside the class.
+*/
+
+#include <iostream>
+#include <string>
+
+template <typename T, typename U, typename V>
+class Triad
+{
+public:
+    Triad(const T& x, const U& y, const V& z)
+        : m_x{ x }
+        , m_y{ y }
+        , m_z{ z }
+    {
+    }
+    const T& GetX() const { return m_x; }
+    const U& GetY() const { return m_y; }
+    const V& GetZ() const { return m_z; }
+
+    void Print() const;
+
+private:
+    T m_x{};
+    U m_y{};
+    V m_z{};
+};
+
+template <typename T, typename U, typename V>
+void Triad<T, U, V>::Print() const
+{
+    std::cout << '[' << m_x << ", " << m_y << ", " << m_z << ']';
+}
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    Triad<int, int, int> t1{ 1, 2, 3 };
+    t1.Print();
+    std::cout << '\n';
+    std::cout << t1.GetX() << '\n';
+
+    using namespace std::literals::string_literals;
+    const Triad t2{ 1, 2.3, "Hello"s };
+    t2.Print();
+    std::cout << '\n';
 
     return 0;
 }
