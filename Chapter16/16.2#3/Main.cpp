70e0f3f2d95a6222d58bf828c3dcf491117a3a71
H0RIS@outlook.com <H0RIS@outlook.com>
2025-03-24 15:13:55 +0100

    16.2#3

---
+++ Chapter16/16.2#3/Main.cpp
@@ -0,0 +1,18 @@
+//
+// Define a std::vector (using an explicit template type argument) to hold the high temperature (to the nearest tenth of a degree) for each day of a year (assume 365 days in a year).
+//
+
+import std;
+
+int main()
+{
+    std::vector<double> temperatures(365);
+
+    for (int number : temperatures
+        )
+    {
+        std::cout << number << " ";
+    }
+
+    return 0;
+}
---
+++ Chapter16/16.2#4/Main.cpp
@@ -0,0 +1,12 @@
+//
+
+
+
+import std;
+
+int main()
+{
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
