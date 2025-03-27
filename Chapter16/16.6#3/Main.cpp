aca8d3ab6e3e0c82ed43ef3533725b62dc99769d
H0RIS@outlook.com <H0RIS@outlook.com>
2025-03-27 11:22:48 +0100

    16.6#3 init

---
+++ Chapter16/16.6#3/Main.cpp
@@ -0,0 +1,17 @@
+//
+// Given the solution for quiz 2, do the following:
+// - Ask the user for a value between 1 and 9. If the user does not enter a value between 1 and 9, repeatedly ask for an integer value until they do.
+// If the user enters a number followed by extraneous input, ignore the extraneous input.
+// - Print the array.
+// - Write a function template to search the array for the value that the user entered.If the value is in the array, return the index of that element.If the value is not in the array, return an appropriate value.
+// - If the value was found, print the value and index.If the value was not found, print the value and that it was not found.
+//
+
+import std;
+
+int main()
+{
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
